c5ee002076b21129482bf39b810c296fa4fe676a
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2025-02-26 10:35:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "28580773")
+    API_HASH = os.environ.get("API_HASH", "f80e465a8805bee5c6bf29fa12d6cc0c")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7794943135:AAG3U8Cu9U3I4EZOGu4WA_o-oToQSHNmYnU") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://telegrambott19:telegrambott19@cluster0.hetez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
 
 
