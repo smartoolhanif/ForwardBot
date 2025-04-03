fb3832442a299ea7ffc2ebf766b1b1dc29cdc5ba
smartoolhanif <clashofkinghanif44@gmail.com>
2025-04-03 12:23:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = os.environ.get("API_ID", "10253517")
     API_HASH = os.environ.get("API_HASH", "65222e5b2950c2a486ac61964123b83a")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7722659903:AAE7FvrveuF0UTetvMa1SF-2UKGnsrjgzE4") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "viewboosterhanif_bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://ckinghanif:l5jY7NXB9kELEHlQ@cluster0.halfsnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "Cluster0")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5260441331').split()]
